1b5197ecadb7d14109516f37dfcaeb7b6b10d36e
Tony <abusch@css.edu>
2021-10-21 23:49:16 -0500

    second lesson

---
+++ app.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+const generatePage = require('./src/page-template.js')
+
+fs.writeFile('index.html',generatePage(name, github),err => {
+    if (err) throw new err;
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});  
