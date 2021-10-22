4105db69b3925dbf4db5c56b73f774950554cdac
Tony <abusch@css.edu>
2021-10-22 00:10:21 -0500

    install modules

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ const generatePage = require('./src/page-template.js')
 fs.writeFile('index.html',generatePage(name, github),err => {
     if (err) throw new err;
     console.log('Portfolio complete! Check out index.html to see the output!')
-});  
+});
